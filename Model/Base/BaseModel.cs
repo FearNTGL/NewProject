ed1897f45a9d59568d61d2e2ba97daa86e5e3c27
Vlad <vladtatarkin@yandex.ru>
2021-04-04 21:44:55 +0700

    Начало создания модели INPC

---
+++ Model/Base/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace BukStat.Model.Base
+{
+    public class BaseModel : INotifyPropertyChanged
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
