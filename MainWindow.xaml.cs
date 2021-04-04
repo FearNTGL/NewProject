7dc914b4b06a12bd104676cf73b5b74b4c7bf06e
Vlad <vladtatarkin@yandex.ru>
2021-04-04 21:28:05 +0700

    Создали проект WPF, почистили мусор, настроили гит, чистый проект.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace BukStat
+{
+    
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
