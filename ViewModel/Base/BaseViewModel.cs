77b9c4e433842e2d5c3deba447d7d031939c133a
Vlad <vladtatarkin@yandex.ru>
2021-04-05 13:46:36 +0700

    Сделан базовый класс и шаблон для INPC

---
+++ ViewModel/Base/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace BukStat.ViewModel.Base
+{
+    public abstract class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string PropertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = "")
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(PropertyName);
+            return true;
+        }
+    }
+}
---
+++ ViewModel/MainWindowsViewModel.cs
@@ -0,0 +1,16 @@
+﻿using BukStat.ViewModel.Base;
+
+
+namespace BukStat.ViewModel
+{
+    class MainWindowsViewModel : BaseViewModel
+    {
+        private string _Title = "BukStat";
+        public string Title
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+
+        }
+    }
+}
